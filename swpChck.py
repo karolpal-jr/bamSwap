39e2d06a6f212ee903678bbcbf556d1b8f0b1d4f
Karol Pal <karolpal.jr@gmail.com>
2017-08-02 23:16:52 +0200

    download seq from coordinates

--- swpChck.py
+++ swpChck.py
@@ -2,8 +2,31 @@ import subprocess
 import pysam
 import sys
 import pandas
+import matplotlib
 
-COORDINATES = "./SNP/all.snps.sorted.bed"
+COORDINATES = "./SNP/all.snps.sorted.txt"
+
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+
 
 #proc = subprocess.run(["which", "samtools"], stdout=subprocess.PIPE)
 
@@ -24,15 +47,16 @@ else:
 #read bed file
 
 coord = pandas.read_csv(COORDINATES, sep='\t',header=None)
-out = coord[[3]]
+index = coord[4].tolist()
+
+columns=[sys.argv[1:]]
+df = pandas.DataFrame(index=index,columns=columns)
+
+full = len(df)*len(sys.argv[1:])
+inc = 0
 
 
-out.columns=['SNP']
-print(out)
-
-#create dataframe from bed file
-
-
+printProgressBar(inc, full, prefix='Progress:', suffix='Complete', length=50)
 for file in sys.argv[1:]:
     samfile = pysam.Samfile(file, "rb")
     if 'chr' in samfile.references[1]:
@@ -43,19 +67,30 @@ for file in sys.argv[1:]:
         chr = ''
 
     for index,row in coord.iterrows():
+        inc = inc + 1
         #print(row)
-
+        ref = 0
+        all = 0
         for pileupcolumn in samfile.pileup(chr+str(row[0]), row[1], row[2]):
             for pileupread in pileupcolumn.pileups:
-                if pileupcolumn.pos == row[1]:
-                    #print(row[3])
-                    #print("query pos")
+                if pileupcolumn.pos == row[1]: # 0 based coordinates ??
                     if pileupread.query_position != None:
                         base = pileupread.alignment.query_sequence[pileupread.query_position]
-                    #print(base)
+                        all = all + 1
+                        if row[3]== base.upper():
+                            ref = ref + 1
+                        #print(base)
+        printProgressBar(inc, full, prefix='Progress:', suffix='Complete', length=50)
+        if all > 0:
+            df.loc[[row[4]],[file]] = ref/all
+        else:
+            df.loc[[row[4]], [file]] = 0
 
         #add column to dataframe
 
 
 
 #plot graph
+
+plot = df.plot.scatter(x=sys.argv[1], y=sys.argv[2])
+plot.savefig("out.pdf")
